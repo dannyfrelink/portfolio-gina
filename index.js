0931c51c336dbb6496ba86b7a0ec1da033a08db2
dannyfrelink <danny-frelink@hotmail.com>
2022-08-15 19:14:21 +0800

    setup website

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5151;
+// const compression = require('compression');
+// require('dotenv').config();
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require('socket.io');
+// const io = new Server(server);
+// const fetch = require('node-fetch');
+
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.use((req, res) => {
+    res.status(404).send('Sorry, could not find the page you were looking for.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
