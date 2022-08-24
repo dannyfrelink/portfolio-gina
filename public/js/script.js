baf95dc85cf1af599a737a1364bec15d63064159
dannyfrelink <danny-frelink@hotmail.com>
2022-08-24 17:36:01 +0800

    make functioning hamburger menu

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,14 @@
+const hamburgerMenu = document.querySelector('#hamburger_menu');
 const footer = document.querySelector('footer');
 
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('open');
+});
+
 if (location.pathname === '/') {
-    footer.classList.add('tranform_footer')
+    footer.classList.add('tranform_footer');
 }
+
+// if (location.pathname === '/overmij') {
+//     hamburgerMenu
+// }
