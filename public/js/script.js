d59f1e754c48f750e3b0c76471c4f15ee53ba449
dannyfrelink <danny-frelink@hotmail.com>
2022-08-28 13:27:18 +0700

    start styling about page

--- public/js/script.js
+++ public/js/script.js
@@ -6,8 +6,22 @@ hamburgerMenu.addEventListener('click', () => {
 });
 
 if (location.pathname === '/' || location.pathname === '/portfolio') {
-    footer.classList.add('tranform_footer');
+    footer.classList.add('transform_footer');
+} else if (screen.width >= 1000) {
+    footer.classList.add('transform_footer_large');
+} else if (screen.width < 1000) {
+    footer.classList.remove('transform_footer_large');
 }
+
+window.addEventListener('resize', () => {
+    if (location.pathname === '/overmij' || location.pathname === '/tarieven') {
+        if (screen.width >= 1000) {
+            footer.classList.add('transform_footer_large');
+        } else if (screen.width < 1000) {
+            footer.classList.remove('transform_footer_large');
+        }
+    }
+})
 
 // if (location.pathname === '/overmij') {
 //     hamburgerMenu
