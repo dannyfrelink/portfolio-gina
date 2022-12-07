a232b881d7ec9ae07d86dadcf9f45200d56c7f26
dannyfrelink <danny-frelink@hotmail.com>
2022-12-07 15:21:45 +0800

    trying to fix service error vercel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5151;
+const PORT = 5151;
 const fetch = require('node-fetch');
 const compression = require('compression');
 
