6608a1b18732a0e2c07823be7058d2151d0136ee
dannyfrelink <danny-frelink@hotmail.com>
2022-12-12 09:13:47 +0800

    fix bug with not changing url on prices page

--- public/js/setLanguage.js
+++ public/js/setLanguage.js
@@ -1,13 +1,26 @@
 if (location.pathname.includes('/en')) {
     let pathname = location.pathname.split('/en')[1];
+
     if (pathname !== '') {
-        languageSelector.setAttribute('href', pathname);
+        if(location.pathname.includes('hour')) {
+            const newPath = pathname.replace('hour', 'uur');
+            languageSelector.setAttribute(
+                'href', 
+                newPath.includes('uurs') ? newPath.replace('uurs', 'uur') : newPath
+            );
+        }
     } else {
         languageSelector.setAttribute('href', '/');
     }
 } else {
     if (location.pathname !== '/') {
-        languageSelector.setAttribute('href', `/en${location.pathname}`);
+        if(location.pathname.includes('uur')) {
+            const newPath = location.pathname.replace('uur', 'hours');
+            languageSelector.setAttribute(
+                'href', 
+                `/en${newPath.includes('1-hours') ? newPath.replace('hours', 'hour') : newPath}`
+            );
+        }
     } else {
         languageSelector.setAttribute('href', '/en');
 
