3a4e0a44b7a356414535a79c23cbe13aad699794
dannyfrelink <danny-frelink@hotmail.com>
2023-01-20 15:37:38 +0800

    update: renew link for contact form

--- index.js
+++ index.js
@@ -1,99 +1,98 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5151;
-const fetch = require('node-fetch');
-const compression = require('compression');
+const fetch = require("node-fetch");
+const compression = require("compression");
 
-// Vervangen met https://www.ginatenhoope.com/
 const fetchPortfolioImages = (url) => {
-    return fetch(`${url}/json/portfolio-images.json`)
-        .then(res => res.json())
-        .then(data => Object.values(data))
-        .catch(err => console.log(err));
-}
+  return fetch(`${url}/json/portfolio-images.json`)
+    .then((res) => res.json())
+    .then((data) => Object.values(data))
+    .catch((err) => console.log(err));
+};
 
 app.use(compression());
-app.use(express.static('static'));
-app.set('view engine', 'ejs');
+app.use(express.static("static"));
+app.set("view engine", "ejs");
 
 // Dutch routes
-app.get('/', (req, res) => {
-    res.render('home');
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get('/overmij', (req, res) => {
-    res.render('about');
+app.get("/overmij", (req, res) => {
+  res.render("about");
 });
 
-app.get('/portfolio', async (req, res) => {
-    const protocol = req.protocol;
-    const host = req.headers.host;
-    const url = `${protocol}://${host}`;
-    const portfolioImages = await fetchPortfolioImages(url);
+app.get("/portfolio", async (req, res) => {
+  const protocol = req.protocol;
+  const host = req.headers.host;
+  const url = `${protocol}://${host}`;
+  const portfolioImages = await fetchPortfolioImages(url);
 
-    res.render('portfolio', { portfolioImages });
+  res.render("portfolio", { portfolioImages });
 });
 
-app.get('/portfolio/:image', async (req, res) => {
-    const image = req.params.image;
+app.get("/portfolio/:image", async (req, res) => {
+  const image = req.params.image;
 
-    res.render('en/portfolio-detail', { image });
+  res.render("en/portfolio-detail", { image });
 });
 
-app.get('/tarieven', (req, res) => {
-    res.render('prices');
+app.get("/tarieven", (req, res) => {
+  res.render("prices");
 });
 
-app.get('/tarieven/:pakket', (req, res) => {
-    const package = req.params.pakket.split('-').join(' ');
-    res.render('prices-package', { package });
+app.get("/tarieven/:pakket", (req, res) => {
+  const package = req.params.pakket.split("-").join(" ");
+  res.render("prices-package", { package });
 });
 
-app.get('/bedankt', (req, res) => {
-    res.render('thanks');
+app.get("/bedankt", (req, res) => {
+  res.render("thanks");
 });
 
 // English routes
-app.get('/en', (req, res) => {
-    res.render('en/home');
+app.get("/en", (req, res) => {
+  res.render("en/home");
 });
 
-app.get('/en/overmij', (req, res) => {
-    res.render('en/about');
+app.get("/en/overmij", (req, res) => {
+  res.render("en/about");
 });
 
-app.get('/en/portfolio', async (req, res) => {
-    const protocol = req.protocol;
-    const host = req.headers.host;
-    const url = `${protocol}://${host}`;
-    const portfolioImages = await fetchPortfolioImages(url);
+app.get("/en/portfolio", async (req, res) => {
+  const protocol = req.protocol;
+  const host = req.headers.host;
+  const url = `${protocol}://${host}`;
+  const portfolioImages = await fetchPortfolioImages(url);
 
-    res.render('en/portfolio', { portfolioImages });
+  res.render("en/portfolio", { portfolioImages });
 });
 
-app.get('/en/portfolio/:image', async (req, res) => {
-    const image = req.params.image;
+app.get("/en/portfolio/:image", async (req, res) => {
+  const image = req.params.image;
 
-    res.render('en/portfolio-detail', { image });
+  res.render("en/portfolio-detail", { image });
 });
 
-app.get('/en/tarieven', (req, res) => {
-    res.render('en/prices');
+app.get("/en/tarieven", (req, res) => {
+  res.render("en/prices");
 });
 
-app.get('/en/tarieven/:pakket', (req, res) => {
-    const package = req.params.pakket.split('-').join(' ');
-    res.render('en/prices-package', { package });
+app.get("/en/tarieven/:pakket", (req, res) => {
+  const package = req.params.pakket.split("-").join(" ");
+  res.render("en/prices-package", { package });
 });
 
-app.get('/en/bedankt', (req, res) => {
-    res.render('en/thanks');
+app.get("/en/bedankt", (req, res) => {
+  res.render("en/thanks");
 });
 
 app.use((req, res) => {
-    res.status(404).send('Sorry, could not find the page you were looking for.');
+  res.status(404).send("Sorry, could not find the page you were looking for.");
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
+  console.log(`Listening on port: ${PORT}`);
 });
